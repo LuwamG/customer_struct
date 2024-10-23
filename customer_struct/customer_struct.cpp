d43bd15e111ecff1bcf2729f2e1009943741c167
LuwamG <30065081@cc.peralta.edu>
2024-10-23 01:00:34 -0700

    update code

--- customer_struct/customer_struct.cpp
+++ customer_struct/customer_struct.cpp
@@ -7,20 +7,20 @@ using namespace std;
 //Function to check the characters
 bool CustomerNumber::isValid() const {
     if (number.length() != 6) {
-        return false; // Length must be exactly 6
+        return false; 
     }
 
     // Checking the first two characters are alphabetic
     for (int i = 0; i < 2; ++i) {
-        if (!std::isalpha(number[i])) {
+        if (!isalpha(number[i])) {
             return false;
         }
     }
 
     // Checking the last four characters are digits
     for (int i = 2; i < 6; ++i) {
-        if (!std::isdigit(number[i])) {
-            return false; // Not a digit
+        if (!isdigit(number[i])) {
+            return false; 
         }
     }
 
